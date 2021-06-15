3d5ddebb7caa3067de95f18474de994b063da389
gokul-imaginea <83589864+gokul-imaginea@users.noreply.github.com>
2021-06-15 16:04:18 +0530

    Update install_req.sh

--- install_req.sh
+++ install_req.sh
@@ -1,4 +1,8 @@
-# Clone 2 repos.
+command -v conda >/dev/null 2>&1 || { echo >&2 "conda not found. Please refer to the README and install Miniconda."; exit 1; }
+command -v git >/dev/null 2>&1 || { echo >&2 "git not found. Please refer to the README and install Git."; exit 1; }
+
+source $(conda info --base)/etc/profile.d/conda.sh
+
 git clone https://github.com/facebookresearch/detectron2.git
 conda create -n alsome1 python==3.7 -y
 conda activate alsome1
