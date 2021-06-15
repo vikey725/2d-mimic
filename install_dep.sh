a59d103709b934b94fb2379a0eba6873dd5a8b44
gokul-imaginea <83589864+gokul-imaginea@users.noreply.github.com>
2021-06-15 17:13:24 +0530

    Delete install_dep.sh

--- install_dep.sh
+++
@@ -1,6 +0,0 @@
-cd v4l2loopback
-make install
-sudo apt-get install v4l2loopback-utils
-sudo apt-get install python3-opencv 
-sudo apt-get install ffmpeg  
-sudo modprobe v4l2loopback video_nr=7 card_label="Also-Me" exclusive_caps=1
